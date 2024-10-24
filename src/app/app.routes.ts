029659437834a5721df88a9414e551bdcaa2d4b3
yaromochka <137483082+yaromochka@users.noreply.github.com>
2024-10-24 22:31:09 +0300

    fix: page sizing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {AuthPageComponent} from '../pages/auth-page/auth-page.component';
 import {AppComponent} from './app.component';
 import {WeatherPageComponent} from '../pages/weather-page/weather-page.component';
 import {PagenotfoundComponent} from '../pages/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
+
   {
     path: 'auth', component: AuthPageComponent
   },
